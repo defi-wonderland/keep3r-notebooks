5fb582679923a981a8964803c940127b43aa71c2
wei3erHase <wei3erhase@defi.sucks>
2021-08-23 15:34:18 -0300

    style: added comments in work-n-burn

--- notebooks/.exports/work-n-burn.js
+++ notebooks/.exports/work-n-burn.js
@@ -15,48 +15,44 @@ next(async () => {
   await $.setup();
 });
 
-var liquidityPool, liquidityWhale;
-next(async () => {
-  const data = await $.setupLiquidity(LIQUIDITIES.KP3R_WETH);
-  liquidityPool = data.pool;
-  liquidityWhale = data.whale;
-});
-
 pool = '0x11b7a6bc0259ed6cf9db8f499988f9ecc7167bf5';
-next(async () => {
-  uniPool = await $.fetch('IUniswapV3Pool', pool);
-
-  await uniPool.increaseObservationCardinalityNext(0x00ff);
-  kp3rPool = await $.deploy('Keep3rV1Pair', pool);
-});
-
 WETH9 = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
 router = '0xE592427A0AEce92De3Edee1F18E0157C05861564';
 
 next(async () => {
+  uniPool = await $.fetch('IUniswapV3Pool', pool);
+
+  kp3rPool = await $.deploy('Keep3rV1Pair', pool);
   uniRouter = await $.fetch('ISwapRouter', router);
   weth = await $.fetch('WETH9', WETH9);
+
+  // increase cardinality of the pool
+  await uniPool.increaseObservationCardinalityNext(0x00ff);
 });
 
 next(async () => {
-  await $.keep3r.connect($.governance).approveLiquidity(kp3rPool.address);
-
+  // fund the jobOwner
   await weth.connect($.jobOwner).deposit({ value: toUnit(100) });
   await $.keep3rV1Proxy.connect($.governance)['mint(address,uint256)']($.jobOwner.address, toUnit(0xffff));
 
-  await weth.connect($.jobOwner).approve(kp3rPool.address, toUnit(0xffff));
-  await $.keep3rV1.connect($.jobOwner).approve(kp3rPool.address, toUnit(0xffff));
+  // approve spendings
+  await weth.connect($.jobOwner).approve(kp3rPool.address, toUnit(0xffffff));
+  await $.keep3rV1.connect($.jobOwner).approve(kp3rPool.address, toUnit(0xffffff));
+  await $.keep3rV1.connect($.jobOwner).approve(uniRouter.address, toUnit(0xffffff));
 
+  // mint a position
   await kp3rPool.connect($.jobOwner).mint(toUnit(100), toUnit(5.2), toUnit(90), toUnit(4), $.jobOwner.address);
 
   console.log('LPs', (await kp3rPool.balanceOf($.jobOwner.address)).div(toUnit(1)).toNumber());
 
-  await $.keep3rV1.connect($.jobOwner).approve(uniRouter.address, toUnit(0xffffff));
+  // make an empty swap
   await uniRouter.connect($.jobOwner).exactInputSingle([$.keep3rV1.address, WETH9, 10000, $.jobOwner.address, 2638281696, 1, 0, 0]);
   await $.sleep($.time(5, 'days'));
 });
 
 next(async () => {
+  await $.keep3r.connect($.governance).approveLiquidity(kp3rPool.address);
+
   await kp3rPool.approve($.keep3r.address, toUnit(0xffff));
   await $.keep3r.connect($.jobOwner).addLiquidityToJob($.job.address, kp3rPool.address, toUnit(2));
   console.log('liquidityAmount', (await $.keep3r.liquidityAmount($.job.address, kp3rPool.address)).div(toUnit(1)).toNumber());
@@ -64,7 +60,6 @@ next(async () => {
 
 next(async () => {
   console.log('Start of simulation');
-  //console.log(await $.helper.observe(kp3rPool.address,[0,432000]))
 
   // sleep some hours
   await $.sleepAndRecord($.time(2, 'weeks'), $.time(4, 'hours'));
@@ -76,8 +71,9 @@ next(async () => {
     await $.sleepAndRecord($.time(0.5, 'days'), $.time(12, 'hours'));
     bonds = await $.keep3r.bonds($.keeper.address, $.keep3rV1.address);
     if (bonds.gt(toUnit(20))) {
-      console.log('burn', bonds.div(toUnit(1)).toNumber());
+      console.log('swaps', bonds.div(toUnit(1)).toNumber(), 'KP3R');
       await $.keep3r.connect($.keeper).unbond($.keep3rV1.address, bonds);
+      // somebody swaps 20 KP3R into the pool
       await uniRouter
         .connect($.jobOwner)
         .exactInputSingle([$.keep3rV1.address, WETH9, 10000, $.keeper.address, 2638281696, bonds, toUnit(0.00001), 0]);
@@ -86,7 +82,6 @@ next(async () => {
 
   await $.sleepAndRecord($.time(2, 'weeks'), $.time(4, 'hours'));
 
-  //console.log(await $.helper.observe(kp3rPool.address,[0,432000]))
   console.log('End of simulation');
 });
 
